cb8300eed6307d4d6c3e643ab09020ce0f12bb60
ViralSoni104 <47710626+ViralSoni104@users.noreply.github.com>
2021-03-10 12:10:34 +0530

    Updated

--- GeekysDesk/GeekysDesk/settings.py
+++ GeekysDesk/GeekysDesk/settings.py
@@ -141,15 +141,8 @@ SOCIAL_AUTH_FACEBOOK_EXTRA_DATA = [
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
